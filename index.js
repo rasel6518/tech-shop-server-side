16ae03a955d06b8a1b680d5a739c36662f62543b
rasel khan <tunnerbd2016@gmail.com>
2023-10-19 21:55:53 +0600

    newproductdb collection add

--- index.js
+++ index.js
@@ -31,10 +31,19 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+
         const brandCollection = client.db('brandsDB').collection('brands')
+        const newCollection = client.db('newProductDB').collection('newProducts')
+
+
 
         app.get('/brands', async (req, res) => {
             const cursor = brandCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        app.get('/newProducts', async (req, res) => {
+            const cursor = newCollection.find();
             const result = await cursor.toArray();
             res.send(result)
         })
