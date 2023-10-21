51326681d155bf83c642d027d3f6ce17678d0f86
rasel khan <tunnerbd2016@gmail.com>
2023-10-21 20:42:53 +0600

    final revision

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_PASS}@cluster0.j6xnqa4.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://brandsproduct:FbVGpB14qyIU1kMJ@cluster0.j6xnqa4.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6xnqa4.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
